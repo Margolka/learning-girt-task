6c9d0d2f64aec2e7fa7d8dc17a0caa2e0454f343
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-23 15:11:16 +0100

    Print items number

--- task.py
+++ task.py
@@ -3,3 +3,4 @@ total_items=0
 for key, value in shopping_list.items():
   print(f"Idę do {key.capitalize()}, kupuję tu następujące rzeczy:{[x.capitalize() for x in value]}")
   total_items=total_items+len(value)
+print(f"W sumie kupuję {total_items} produktów")
