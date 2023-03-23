cc50bc20d6792caa34d454944bd0fd01fd0a23e8
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-23 15:04:45 +0100

    Print shopping list

--- task.py
+++ task.py
@@ -1,1 +1,3 @@
 shopping_list={"piekarnia":['chleb', 'pączek', 'bułki'],"warzywniak":['marchew', 'seler', 'rukola','brukselka']}
+for key, value in shopping_list.items():
+  print(f"Idę do {key.capitalize()}, kupuję tu następujące rzeczy:{[x.capitalize() for x in value]}")
