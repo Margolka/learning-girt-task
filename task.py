3556453f8f7dba175c2a5f8c927c451eab0d52f0
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-23 15:06:39 +0100

    Add counting items

--- task.py
+++ task.py
@@ -1,3 +1,5 @@
 shopping_list={"piekarnia":['chleb', 'pączek', 'bułki'],"warzywniak":['marchew', 'seler', 'rukola','brukselka']}
+total_items=0
 for key, value in shopping_list.items():
   print(f"Idę do {key.capitalize()}, kupuję tu następujące rzeczy:{[x.capitalize() for x in value]}")
+  total_items=total_items+len(value)
