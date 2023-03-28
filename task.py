3dbc438f49d6cf8916dc8e4f9658c812128601c7
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-28 20:58:35 +0200

    Print greeting

--- task.py
+++ task.py
@@ -1,6 +1,1 @@
-shopping_list={"piekarnia":['chleb', 'pączek', 'bułki'],"warzywniak":['marchew', 'seler', 'rukola','brukselka'],"drogeria":['krem do twarzy','shampon','pasta do zębów'],'apteka':['aspiryna']}
-total_items=0
-for key, value in shopping_list.items():
-  print(f"Idę do {key.capitalize()}, kupuję tu następujące rzeczy:{[x.capitalize() for x in value]}")
-  total_items=total_items+len(value)
-print(f"W sumie kupuję {total_items} produktów")
+print("Nic tak nie rujnuje piątku, jak świadomość, że jest wtorek")
