065c5b0f5084fb069601682a9cc20d1560322d61
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-28 18:40:12 +0200

    Added Drogeria

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-shopping_list={"piekarnia":['chleb', 'pączek', 'bułki'],"warzywniak":['marchew', 'seler', 'rukola','brukselka']}
+shopping_list={"piekarnia":['chleb', 'pączek', 'bułki'],"warzywniak":['marchew', 'seler', 'rukola','brukselka'],"drogeria":['krem do twarzy','shampon','pasta do zębów']}
 total_items=0
 for key, value in shopping_list.items():
   print(f"Idę do {key.capitalize()}, kupuję tu następujące rzeczy:{[x.capitalize() for x in value]}")
